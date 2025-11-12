858084a85b28dc5a87b6694706f731e06533e69f
Muhammad Kashif <muhammadkashif841@gmail.com>
2025-11-12 10:33:03 +0500

    Update API model name in app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ from PIL import Image
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load Google Gemini Pro Vision API And get response
+## Function to load Google Gemini Vision API And get response
 
 def get_gemini_repsonse(input,image,prompt):
-    model=genai.GenerativeModel('gemini-pro-vision')
+    model=genai.GenerativeModel('gemini-2.5-pro')
     response=model.generate_content([input,image[0],prompt])
     return response.text
 
